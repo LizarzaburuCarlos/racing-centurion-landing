b90e3878b171df688cb91b83196258c9cb6e77d2
Carlos Lizarzaburu <carlos_abanto2@hotmail.com>
2023-08-24 17:44:03 -0500

    versión prueba

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,21 @@
-
+import "../styles/Navbar.styles.css"
 
 export const Navbar = () => {
     return (
-        <nav className="w-full h-[10vh] bg-slate-600 text-center">
-            <h1>Navbar</h1>
+        <nav className="navbar w-full h-16 lg:h-20 z-10 flex justify-center items-center">
+            <div className="navbar__container 2xl:w-[1280px] xl:w-10/12 w-11/12 flex justify-between align-center">
+                <div className="navbar__logo">
+                    <a href="/#home" aria-label="Home" className="navbar__home">
+                        <img src="/img/logo_placeholder.png" alt="Logo placeholder" />
+                    </a>
+                </div>
+                <div className="navbar__links flex gap-10 text-white items-center">
+                    <a href="/" className="navbar__link">Link 1</a>
+                    <a href="/" className="navbar__link">Link 2</a>
+                    <a href="/" className="navbar__link">Link 3</a>
+                    <a href="/" className="navbar__link">Link 4</a>
+                </div>
+            </div>
         </nav>
     )
 }
